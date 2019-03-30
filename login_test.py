7aabba82c641100e4665d29ca902395c7448f597
cleborys <borys@math.ku.dk>
2019-03-30 20:44:51 +0100

    refactor test_canResetPassword. Password policies seem not to be enforced in Firefox, see issue on password policies

--- login_test.py
+++ login_test.py
@@ -33,44 +33,19 @@ def test_canLoginWithGoodCredentials(dummy_user):
 
 def test_canResetPassword(dummy_user):
     global dummy_user_email, dummy_user_password
-    driver = webdriver.Firefox()
-    driver.get(pages["login"])
+    for driver_constructor in driver_constructor_generator():
+        with driver_constructor() as driver:
+            request_password_reset(driver, dummy_user_email)
 
-    forgot_password_link = driver.find_element_by_xpath("//p[@class='forgot']/a")
-    forgot_password_link.click()
+            db_connection = get_database_connection()
+            cursor = db_connection.cursor()
+            user_id = get_dummy_userid(cursor)
+            reset_token = get_password_reset_token_by_userid(user_id, cursor)
 
-    WebDriverWait(driver, 10).until(EC.url_changes(pages["login"]))
+            enter_password_request_token(driver, reset_token)
 
-    email_entry = driver.find_element_by_id("input_username")
-    email_entry.send_keys(dummy_user_email)
-    email_entry.submit()
+            new_password = dummy_user_password + "notSame"
+            enter_new_password_for_reset(driver, new_password)
 
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_reset-token")))
-
-    # wait for token to be written to database
-    time.sleep(0.5) 
-
-    db_connection = get_database_connection()
-    cursor = db_connection.cursor()
-    user_id = get_dummy_userid(cursor)
-    reset_token = get_password_reset_token_by_userid(user_id, cursor)
-
-    reset_token_entry = driver.find_element_by_id("input_reset-token")
-    reset_token_entry.send_keys(reset_token)
-    reset_token_entry.submit()
-
-    new_password = dummy_user_password + "butDifferent"
-
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_new_password")))
-    password_entry = driver.find_element_by_id("input_new_password")
-    password_entry.send_keys(new_password)
-
-    password_confirm = driver.find_element_by_id("input_confirm_password")
-    password_confirm.send_keys(new_password)
-    password_confirm.submit()
-
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_username")))
-
-    try_login(driver, dummy_user_email, new_password)
-    assert "<title>Login</title>" not in driver.page_source
-    driver.quit()
+            try_login(driver, dummy_user_email, new_password)
+            assert "<title>Login</title>" not in driver.page_source
--- navigation.py
+++ navigation.py
@@ -9,7 +9,6 @@ pages["root"] = "http://kbhff.local/"
 pages["login"] = pages["root"] + "login"
 
 def try_login(driver, username, password):
-
     driver.get(pages["login"])
 
     login_url = driver.current_url
@@ -25,3 +24,41 @@ def try_login(driver, username, password):
     time.sleep(0.5)
     # wait for page to load, up to ten seconds
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//html")))
+
+def request_password_reset(driver, user_email):
+    driver.get(pages["login"])
+
+    forgot_password_link = driver.find_element_by_xpath("//p[@class='forgot']/a")
+    forgot_password_link.click()
+
+    WebDriverWait(driver, 10).until(EC.url_changes(pages["login"]))
+
+    email_entry = driver.find_element_by_id("input_username")
+    email_entry.send_keys(user_email)
+    email_entry.submit()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_reset-token")))
+
+    # wait for token to be written to database
+    time.sleep(0.5) 
+
+def enter_password_request_token(driver, reset_token):
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_reset-token")))
+
+    reset_token_entry = driver.find_element_by_id("input_reset-token")
+    reset_token_entry.send_keys(reset_token)
+    reset_token_entry.submit()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_new_password")))
+
+def enter_new_password_for_reset(driver, new_password):
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_new_password")))
+    password_entry = driver.find_element_by_id("input_new_password")
+    password_entry.send_keys(new_password)
+
+    password_confirm = driver.find_element_by_id("input_confirm_password")
+    password_confirm.send_keys(new_password)
+    password_confirm.submit()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_username")))
+
