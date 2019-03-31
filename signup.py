8c61598b945bda13b8b1fb27088e7504b3541ba9
cleborys <borys@math.ku.dk>
2019-03-31 13:39:53 +0200

    implemented signup_skipping_verification_step

--- signup.py
+++ signup.py
@@ -1,36 +1,40 @@
 from navigation import *
 
+dummy_card = {}
+dummy_card["number"] = "4242 4242 4242 4242"
+dummy_card["MM"] = "12"
+dummy_card["YY"] = "20"
+dummy_card["CVC"] = "123"
+
+def random_string(n):
+    import string, random
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+
+def random_user_data():
+    import random
+    user_data = {}
+    user_data["email"] = random_string(10) + "@kbhff.dk"
+    user_data["firstname"] = random_string(10)
+    user_data["lastname"] = random_string(10)
+    user_data["password"] = random_string(15)
+    user_data["department"] = random.choice(["Vesterbro", "Amager"])
+
+    return user_data 
+
 
 def signup_skipping_verification_step(user_data):
     """Create a user by going through the signup flow, but skipping the verification step.
 
     Positional arguments:
         user_data -- a dictionary containing the following keys:
+                        firstname
+                        lastname
                         email
                         password
-                        (others? address etc.)
+                        department ("Vesterbro" or "Amager")
 
     This method uses its own selenium driver, so doesn't disturb the flow of the enclosing test"""
     with webdriver.Firefox() as driver:
-        navigate_to_page("signup", driver)
-
-
-if __name__ == "__main__":
-    import random, string
-    with webdriver.Firefox() as driver:
-        user_data = {}
-        user_data["email"] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)) + "@kbhff.dk"
-        user_data["firstname"] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-        user_data["lastname"] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-        user_data["password"] = "password"
-        user_data["department"] = "Vesterbro"
-
-        dummy_card = {}
-        dummy_card["number"] = "4242 4242 4242 4242"
-        dummy_card["MM"] = "12"
-        dummy_card["YY"] = "20"
-        dummy_card["CVC"] = "123"
-
         navigate_to_page("signup", driver)
         click_button(driver, class_name = "button.primary")
         fill_form_field(user_data["firstname"], driver, form_id="input_firstname")
@@ -44,12 +48,18 @@ if __name__ == "__main__":
 
         click_button(driver, class_name = "skip") #skips verification step
         
+        global dummy_card
         fill_form_field(dummy_card["number"], driver, form_id="input_card_number")
         fill_form_field(dummy_card["MM"], driver, form_id="input_card_exp_month")
         fill_form_field(dummy_card["YY"], driver, form_id="input_card_exp_year")
         fill_form_field(dummy_card["CVC"], driver, form_id="input_card_cvc")
         submit_form(driver)
+        
+        wait_for_next_page(driver)
+        assert("er godkendt" in driver.page_source) # signup and payment were successful
+
+if __name__ == "__main__":
+    signup_skipping_verification_step(random_user_data())
 
 
 
-        time.sleep(10)
