fd1106ed7200abbd5211c1abe06723bce684a7ac
cleborys <borys@math.ku.dk>
2019-04-01 21:49:30 +0200

    Signup flow with and without activation right away

--- signup.py
+++ signup.py
@@ -1,4 +1,5 @@
 from navigation import *
+from check_email import *
 
 dummy_card = {}
 dummy_card["number"] = "4242 4242 4242 4242"
@@ -21,20 +22,7 @@ def random_user_data():
 
     return user_data 
 
-
-def signup_skipping_verification_step(user_data):
-    """Create a user by going through the signup flow, but skipping the verification step.
-
-    Positional arguments:
-        user_data -- a dictionary containing the following keys:
-                        firstname
-                        lastname
-                        email
-                        password
-                        department ("Vesterbro" or "Amager")
-
-    This method uses its own selenium driver, so doesn't disturb the flow of the enclosing test"""
-    with webdriver.Firefox() as driver:
+def _signup_step_data_entry(driver, user_data):
         navigate_to_page("signup", driver)
         click_button(driver, class_name = "button.primary")
         fill_form_field(user_data["firstname"], driver, form_id="input_firstname")
@@ -46,8 +34,7 @@ def signup_skipping_verification_step(user_data):
         check_checkbox(driver, "input_terms")
         submit_form(driver)
 
-        click_button(driver, class_name = "skip") #skips verification step
-        
+def _signup_step_betaling(driver):
         global dummy_card
         fill_form_field(dummy_card["number"], driver, form_id="input_card_number")
         fill_form_field(dummy_card["MM"], driver, form_id="input_card_exp_month")
@@ -59,16 +46,51 @@ def signup_skipping_verification_step(user_data):
         wait_for_next_page(driver)
         assert("er godkendt" in driver.page_source) # signup and payment were successful
 
+def _signup_step_verification(email, driver):
+        mail = get_latest_mail_to(email)
+        activation_code = get_activation_code_from_email(mail.body)
+        fill_form_field(activation_code, driver, form_id="input_verification_code")
+        submit_form(driver)
+
+def signup_via_webform(user_data, activate_right_away=True):
+    """Create a user by going through the signup flow.
+
+    Positional arguments:
+        user_data -- a dictionary containing the following keys:
+                        firstname
+                        lastname
+                        email
+                        password
+                        department ("Vesterbro" or "Amager")
+
+    This method uses its own selenium driver, so doesn't disturb the flow of the enclosing test"""
+    with webdriver.Firefox() as driver:
+        _signup_step_data_entry(driver, user_data)
+
+        if activate_right_away:
+            time.sleep(10)# wait for email
+            _signup_step_verification(user_data["email"], driver)
+        else:
+            click_button(driver, class_name = "skip") #skips verification step
+
+        _signup_step_betaling(driver)
+        
+
 if __name__ == "__main__":
+
     user_data = random_user_data()
-    signup_skipping_verification_step(user_data)
+    signup_via_webform(user_data)
+    with webdriver.Firefox() as driver:
+        try_login(driver, user_data["email"], user_data["password"])
+        time.sleep(10)
+
+    user_data = random_user_data()
+    signup_via_webform(user_data, activate_right_away=False)
     with webdriver.Firefox() as driver:
         try_login(driver, user_data["email"], user_data["password"])
         time.sleep(5)
-        from check_email import *
-        mail = get_latest_mail_to(user_data["email"])
-        print(mail.title)
-        print(mail.body)
-
-
-
+        _signup_step_verification(user_data["email"], driver)
+        submit_form(driver)
+    with webdriver.Firefox() as driver:
+        try_login(driver, user_data["email"], user_data["password"])
+        time.sleep(10)
