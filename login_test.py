74ffd032c4c7cccda417693f57b220a3dc678f74
cleborys <borys@math.ku.dk>
2019-04-09 21:31:34 +0200

    rewrite password reset test to use new api

--- login_test.py
+++ login_test.py
@@ -35,17 +35,28 @@ def test_canResetPassword(dummy_user):
     global dummy_user_email, dummy_user_password
     for driver_constructor in driver_constructor_generator():
         with driver_constructor() as driver:
-            request_password_reset(driver, dummy_user_email)
+            navigate_to_page("login", driver)
 
-            db_connection = get_database_connection()
-            cursor = db_connection.cursor()
-            user_id = get_dummy_userid(cursor)
-            reset_token = get_password_reset_token_by_userid(user_id, cursor)
+            forgot_password_link = driver.find_element_by_xpath("//p[@class='forgot']/a") 
+            forgot_password_link.click()
 
-            enter_password_request_token(driver, reset_token)
+            email_entry = driver.find_element_by_id("input_username")
+            email_entry.send_keys(user_email)
+            email_entry.submit()
+
+            time.sleep(3) # wait for email to be sent
+            mail = get_latest_mail_to(dummy_user_email)
+            reset_token = get_password_reset_code_from_email(mail.body)
+
+            fill_form_field(reset_token, driver, form_id="input_reset-token")
+            submit_form(driver)
 
             new_password = dummy_user_password + "notSame"
             enter_new_password_for_reset(driver, new_password)
+
+            fill_form_field(new_password, driver, form_id="input_new_password")
+            fill_form_field(new_password, driver, form_id="input_confirm_password")
+            submit_form(driver)
 
             try_login(driver, dummy_user_email, new_password)
             assert "<title>Login</title>" not in driver.page_source
