d597da88d1eff12c10f9acb44f8231b4d6007c17
cleborys <borys@math.ku.dk>
2019-04-12 16:53:54 +0200

    uncommented first login test

--- login_test.py
+++ login_test.py
@@ -7,27 +7,27 @@ from check_email import *
 import time
 
 
-## Test specification number 1
-#def test_unverifiedUserFirstLogin(driver, unverified_user_via_medlemshjaelp):
-#    user = unverified_user_via_medlemshjaelp
-#
-#    try_login(driver, user["email"], user["password"])
-#    assert_current_page_is("bekraeft_konto", driver)
-#
-#    mail = get_latest_mail_to(user["email"], expect_title="Aktiver din konto hos KBHFF", retry=5)
-#    token = get_activation_code_from_email(mail.body)
-#    fill_form_field(token, driver, form_id="input_verification_code")
-#    submit_form(driver)
-#    assert_current_page_is("opret_password", driver)
-#
-#    fill_form_field(user["password"], driver, form_id="input_new_password")
-#    fill_form_field(user["password"], driver, form_id="input_confirm_password")
-#    submit_form(driver)
-#    assert_current_page_is("kvittering", driver)
-#
-#    navigate_to_page("login", driver)
-#    assert_current_page_is("login", driver)
-#    assert get_form_field_value(driver, form_id="input_username") == user["email"]
+# Test specification number 1
+def test_unverifiedUserFirstLogin(driver, unverified_user_via_medlemshjaelp):
+    user = unverified_user_via_medlemshjaelp
+
+    try_login(driver, user["email"], user["password"])
+    assert_current_page_is("bekraeft_konto", driver)
+
+    mail = get_latest_mail_to(user["email"], expect_title="Aktiver din konto hos KBHFF", retry=5)
+    token = get_activation_code_from_email(mail.body)
+    fill_form_field(token, driver, form_id="input_verification_code")
+    submit_form(driver)
+    assert_current_page_is("opret_password", driver)
+
+    fill_form_field(user["password"], driver, form_id="input_new_password")
+    fill_form_field(user["password"], driver, form_id="input_confirm_password")
+    submit_form(driver)
+    assert_current_page_is("kvittering", driver)
+
+    navigate_to_page("login", driver)
+    assert_current_page_is("login", driver)
+    assert get_form_field_value(driver, form_id="input_username") == user["email"]
 
 
 # Test specification number 2
