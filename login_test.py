6a6f7e27b56ca2ab30b0841f6653cfbf7fa17e8a
Clemens Borys <borys@math.ku.dk>
2019-04-18 09:48:44 +0200

    login test two abstractions

--- login_test.py
+++ login_test.py
@@ -18,8 +18,7 @@ def test_unverifiedUserFirstLogin(driver, unverified_user_via_medlemshjaelp):
     assert_current_page_is("kvittering", driver)
 
     navigate_to_page("login", driver)
-    assert_current_page_is("login", driver)
-    assert get_form_field_value(driver, form_id="input_username") == user["email"]
+    assert_username_prefilled(user["email"], driver)
 
 
 # Test specification number 2
@@ -32,26 +31,18 @@ def test_unverifiedUserFirstLoginDuplicateTab(driver, unverified_user_via_medlem
     driver.switch_to.window(first_tab)
     try_login(driver, user["email"], user["password"])
     assert_current_page_is("bekraeft_konto", driver)
-    mail = get_latest_mail_to(user["email"], expect_title="Aktiver din konto hos KBHFF", retryCount=5)
-    token = get_activation_code_from_email(mail.body)
+    token = get_verification_token_on_first_login(user["email"])
 
     driver.switch_to.window(second_tab)
     navigate_to_page("bekraeft_konto", driver)
-    assert_current_page_is("bekraeft_konto", driver)
-    fill_form_field(token, driver, form_id="input_verification_code")
-    submit_form(driver)
-    assert_current_page_is("opret_password", driver)
-    fill_form_field(user["password"], driver, form_id="input_new_password")
-    fill_form_field(user["password"], driver, form_id="input_confirm_password")
-    submit_form(driver)
+    input_verification_token(token, driver)
+    create_password(user["password"], driver)
     assert_current_page_is("kvittering", driver)
     navigate_to_page("login", driver)
-    assert_current_page_is("login", driver)
-    assert get_form_field_value(driver, form_id="input_username") == user["email"]
+    assert_username_prefilled(user["email"], driver)
 
     driver.switch_to.window(first_tab)
-    fill_form_field(token, driver, form_id="input_verification_code")
-    submit_form(driver)
+    input_verification_token(token, driver)
     assert_current_page_is("login", driver)
-    assert get_form_field_value(driver, form_id="input_username") == user["email"]
+    assert_username_prefilled(user["email"], driver)
     assert_text_on_page("Din konto er allerede aktiveret!", driver)
