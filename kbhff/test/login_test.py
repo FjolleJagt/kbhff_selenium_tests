0b3d7dcee3e6809f55a5f78ba3e9c4dc95737bff
Clemens Borys <borys@math.ku.dk>
2019-04-22 05:09:41 +0200

    Added Login Test 12

--- kbhff/test/login_test.py
+++ kbhff/test/login_test.py
@@ -153,3 +153,13 @@ def test_validLogin(driver, verified_user):
 
     assert_current_page_is("min_side", driver)
 
+#Test specification number 12
+def test_nonuserLogin(driver):
+    user = random_user_data()
+
+    try_login(driver, user["email"], user["password"])
+
+    assert_current_page_is("login", driver)
+    assert_username_not_prefilled(driver)
+    assert_text_on_page("Det indtastede brugernavn findes ikke i vores system.", driver)
+
